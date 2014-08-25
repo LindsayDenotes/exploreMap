72aa5ce1b8a987def101fad94eedd124a8276f84
Lindsay Brown <LindsayBrown81@users.noreply.github.com>
2014-08-25 19:24:38 -0400

    ID,class,and onclick attribute assigned to each DOM el. onclick doesn't yet point to a script method in the JS file; its still just an alert. JSON is still array. May consider changing to object again. Thinking that if I assign array ID (the index #) to each DOM el, I could return the matched element back to the clicked on element. Seems circuitous, though. That's why I'm considering changing array to object.

--- exploreMap.js
+++ exploreMap.js
@@ -1,13 +1,34 @@
+
+function userClicked(getVal){
+        alert('user clicked: ' + getVal);
+        return getVal;
+        }
+
 $(document).ready(function(){
-    //**can i put a function here $(clicked on <#g>).ready(function(){...?
+
+/*var curElement = document.SELECTED?Element;
+    function onClick(e) {
+                console.log(e);
+                var outputElement = document.getElementById('output-element');
+    //**can i put a function here $(clicked on <#g>).ready(function(){...?*/
+
+        //$( "#fl" ).bind( "click", function() { //Do I need to write 52 binding functions that will pass unique ID to the getJSON function?
+        //alert( "User clicked on 'fl.'" );
+
+        //});
+
+       //var getState = "nh";
+
 
 		var items = [];//**use object instead of array? http://www.mkyong.com/javascript/how-to-access-json-object-in-javascript
 
 		  $.getJSON("stateInfoList.json", function( data ) {  //get resource function
 			console.log(data);//Since you have array in your JSON, you need to target each instead in for loop below
 
+			    //var getState = abv
+
 			    $.each(data.items, function( key, val ) { //each() Iterate over a jQuery object, executing a function for **each matched element.
-				  console.log( key, val ); //key is jsons index, val is whats inside jsons{ }, members are abv, state, contacts.
+				  console.log( key, val ); //key is jsons index, val is whats inside jsons{ }, members or properties are abv, state, contacts.
 
                 //**Can I specify unique ID in these parameters?
 
@@ -21,6 +42,9 @@ $(document).ready(function(){
 					  "class": "my-new-list",
 					  html: items.join( "" ) //this joins all or **selected element(s)** back into a ~nonformatted~ string.//Put g#id in ("")?//
 					  //join method only works with arrays, not jquery objects.
+
+					  //jill from wwc rec'd i add a stop function in this loop.
+
 					  }).appendTo( "#txtDOT" );//"class": "txtDOT",
 				});//end each() iterator function
 
